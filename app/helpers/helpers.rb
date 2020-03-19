ac67861517dc0faecf59a45cff506513bdcd0f65
kelly r maass <kmaass77627@gmail.com>
2020-03-19 22:28:31 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,6 +2,7 @@ class Helpers
 
   def self.current_user(session)
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # User.find_by_id(session[:user_id])
   end
 
   def self.is_logged_in?(session)
