416315778d50d2cf58847741826039f0a80a3149
kelly r maass <kmaass77627@gmail.com>
2020-03-19 21:10:29 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,8 @@ class ApplicationController < Sinatra::Base
       if @user
         session[:user_id] = @user.id
     redirect to '/account'
-  else 
-    erb :error 
+  else
+    erb :error
   end
 
   get '/account' do
