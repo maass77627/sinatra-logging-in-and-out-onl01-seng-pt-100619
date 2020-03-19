7f2cc52541c2162590a62c8c9996ac31e61cfac1
kelly r maass <kmaass77627@gmail.com>
2020-03-19 21:09:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user User.find_by(username: params[:username])
+      if @user
+        session[:user_id] = @user.id
+    redirect to '/account'
   end
 
   get '/account' do
@@ -24,4 +27,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
